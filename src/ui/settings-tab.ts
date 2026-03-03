12bc765dc63db006ade279f7a8aeafb3e55f7e42
David V. Kimball <david@davidvkimball.com>
2026-03-02 23:24:12 -0800

    chore: Apply 1.11.0 SettingGroup upgrade

--- src/ui/settings-tab.ts
+++ src/ui/settings-tab.ts
@@ -1,6 +1,6 @@
-import { App, PluginSettingTab } from "obsidian";
+import { App, PluginSettingTab , SettingGroup} from "obsidian";
 import CustomSlidesPlugin from "../main";
-import { createSettingsGroup } from "../utils/settings-compat";
+
 import { TransitionType } from "../settings";
 
 export class CustomSlidesSettingTab extends PluginSettingTab {
@@ -18,13 +18,13 @@ export class CustomSlidesSettingTab extends PluginSettingTab {
     containerEl.empty();
 
     // First group (no heading) - all settings related to customizing slides presentation
-    const generalGroup = createSettingsGroup(containerEl, undefined, 'custom-slides');
+    const generalGroup = new SettingGroup(containerEl);
 
-    generalGroup.addSetting((setting) => {
+    generalGroup.addSetting((setting: any) => {
       setting
         .setName("Theme")
         .setDesc("Choose a reveal.js theme for your presentations.")
-        .addDropdown(dropdown => dropdown
+        .addDropdown((dropdown: any) => dropdown
           .addOptions({
             "default": "Default (match Obsidian)",
             "black": "Black",
@@ -41,17 +41,17 @@ export class CustomSlidesSettingTab extends PluginSettingTab {
             "blood": "Blood"
           })
           .setValue(this.plugin.settings.theme)
-          .onChange(async (value) => {
+          .onChange(async (value: any) => {
             this.plugin.settings.theme = value;
             await this.plugin.saveSettings();
           }));
     });
 
-    generalGroup.addSetting((setting) => {
+    generalGroup.addSetting((setting: any) => {
       setting
         .setName("Transition effect")
         .setDesc("Choose a transition effect between slides.")
-        .addDropdown(dropdown => dropdown
+        .addDropdown((dropdown: any) => dropdown
           .addOptions({
             "slide-horizontal": "Slide horizontal (default)",
             "slide-vertical": "Slide vertical",
@@ -59,104 +59,104 @@ export class CustomSlidesSettingTab extends PluginSettingTab {
             "none": "None (instant)"
           })
           .setValue(this.plugin.settings.transition)
-          .onChange(async (value) => {
+          .onChange(async (value: any) => {
             this.plugin.settings.transition = value as TransitionType;
             await this.plugin.saveSettings();
           }));
     });
 
-    generalGroup.addSetting((setting) => {
+    generalGroup.addSetting((setting: any) => {
       setting
         .setName("Use WASD for navigation")
         .setDesc("Use W, A, S, D keys to navigate slides. Q jumps to first slide, E to last.")
-        .addToggle(toggle => toggle
+        .addToggle((toggle: any) => toggle
           .setValue(this.plugin.settings.enableWASD)
-          .onChange(async (value) => {
+          .onChange(async (value: any) => {
             this.plugin.settings.enableWASD = value;
             await this.plugin.saveSettings();
           }));
     });
 
-    generalGroup.addSetting((setting) => {
+    generalGroup.addSetting((setting: any) => {
       setting
         .setName("Hide left navigation arrow")
         .setDesc("Toggle visibility of the left navigation arrow.")
-        .addToggle(toggle => toggle
+        .addToggle((toggle: any) => toggle
           .setValue(this.plugin.settings.hideNavigateLeft)
-          .onChange(async (value) => {
+          .onChange(async (value: any) => {
             this.plugin.settings.hideNavigateLeft = value;
             await this.plugin.saveSettings();
           }));
     });
 
-    generalGroup.addSetting((setting) => {
+    generalGroup.addSetting((setting: any) => {
       setting
         .setName("Hide right navigation arrow")
         .setDesc("Toggle visibility of the right navigation arrow.")
-        .addToggle(toggle => toggle
+        .addToggle((toggle: any) => toggle
           .setValue(this.plugin.settings.hideNavigateRight)
-          .onChange(async (value) => {
+          .onChange(async (value: any) => {
             this.plugin.settings.hideNavigateRight = value;
             await this.plugin.saveSettings();
           }));
     });
 
-    generalGroup.addSetting((setting) => {
+    generalGroup.addSetting((setting: any) => {
       setting
         .setName("Hide up navigation arrow")
         .setDesc("Toggle visibility of the up navigation arrow.")
-        .addToggle(toggle => toggle
+        .addToggle((toggle: any) => toggle
           .setValue(this.plugin.settings.hideNavigateUp)
-          .onChange(async (value) => {
+          .onChange(async (value: any) => {
             this.plugin.settings.hideNavigateUp = value;
             await this.plugin.saveSettings();
           }));
     });
 
-    generalGroup.addSetting((setting) => {
+    generalGroup.addSetting((setting: any) => {
       setting
         .setName("Hide down navigation arrow")
         .setDesc("Toggle visibility of the down navigation arrow.")
-        .addToggle(toggle => toggle
+        .addToggle((toggle: any) => toggle
           .setValue(this.plugin.settings.hideNavigateDown)
-          .onChange(async (value) => {
+          .onChange(async (value: any) => {
             this.plugin.settings.hideNavigateDown = value;
             await this.plugin.saveSettings();
           }));
     });
 
-    generalGroup.addSetting((setting) => {
+    generalGroup.addSetting((setting: any) => {
       setting
         .setName("Hide close button")
         .setDesc("Toggle visibility of the close button.")
-        .addToggle(toggle => toggle
+        .addToggle((toggle: any) => toggle
           .setValue(this.plugin.settings.hideCloseBtn)
-          .onChange(async (value) => {
+          .onChange(async (value: any) => {
             this.plugin.settings.hideCloseBtn = value;
             await this.plugin.saveSettings();
           }));
     });
 
-    generalGroup.addSetting((setting) => {
+    generalGroup.addSetting((setting: any) => {
       setting
         .setName("Left-align lists")
         .setDesc("Toggle to left-align bulleted and numbered lists in presentation mode.")
-        .addToggle(toggle => toggle
+        .addToggle((toggle: any) => toggle
           .setValue(this.plugin.settings.leftAlignBullets)
-          .onChange(async (value) => {
+          .onChange(async (value: any) => {
             this.plugin.settings.leftAlignBullets = value;
             await this.plugin.saveSettings();
           }));
     });
 
-    generalGroup.addSetting((setting) => {
+    generalGroup.addSetting((setting: any) => {
       setting
         .setName("Progress bar height")
         .setDesc("Set the height of the progress bar in pixels.")
-        .addText(text => text
+        .addText((text: any) => text
           .setPlaceholder("Enter height in pixels")
           .setValue(this.plugin.settings.progressHeight.toString())
-          .onChange(async (value) => {
+          .onChange(async (value: any) => {
             const numValue = parseInt(value, 10);
             if (!isNaN(numValue) && numValue >= 0) {
               this.plugin.settings.progressHeight = numValue;
@@ -165,37 +165,37 @@ export class CustomSlidesSettingTab extends PluginSettingTab {
           }));
     });
 
-    generalGroup.addSetting((setting) => {
+    generalGroup.addSetting((setting: any) => {
       setting
         .setName("Use Obsidian font settings")
         .setDesc("Use the same font as your Obsidian text settings in slides.")
-        .addToggle(toggle => toggle
+        .addToggle((toggle: any) => toggle
           .setValue(this.plugin.settings.respectObsidianSettings)
-          .onChange(async (value) => {
+          .onChange(async (value: any) => {
             this.plugin.settings.respectObsidianSettings = value;
             await this.plugin.saveSettings();
           }));
     });
 
-    generalGroup.addSetting((setting) => {
+    generalGroup.addSetting((setting: any) => {
       setting
         .setName("Enable pan")
         .setDesc("Allow clicking and dragging to move slides around.")
-        .addToggle(toggle => toggle
+        .addToggle((toggle: any) => toggle
           .setValue(this.plugin.settings.enablePan)
-          .onChange(async (value) => {
+          .onChange(async (value: any) => {
             this.plugin.settings.enablePan = value;
             await this.plugin.saveSettings();
           }));
     });
 
-    generalGroup.addSetting((setting) => {
+    generalGroup.addSetting((setting: any) => {
       setting
         .setName("Enable zoom")
         .setDesc("Allow using the mouse wheel to zoom in and out of slides.")
-        .addToggle(toggle => toggle
+        .addToggle((toggle: any) => toggle
           .setValue(this.plugin.settings.enableZoom)
-          .onChange(async (value) => {
+          .onChange(async (value: any) => {
             this.plugin.settings.enableZoom = value;
             await this.plugin.saveSettings();
           }));
